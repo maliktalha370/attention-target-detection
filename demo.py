9e261a061d7ae514d93205d39a0cc5167537b6bd
talha zubair <94974993+maliktalha1996@users.noreply.github.com>
2023-01-16 13:31:02 +0500

    Finalizing wrapper

--- demo.py
+++ demo.py
@@ -9,72 +9,61 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from PIL import Image
-# from scipy.misc import imresize
 from cv2 import resize as imresize
 from model import ModelSpatial
 from utils import imutils, evaluation
-from config import *
+
 import cv2
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--model_weights', type=str, help='model weights', default='model_demo.pt')
-parser.add_argument('--image_dir', type=str, help='images', default='data/demo/frames')
-parser.add_argument('--head', type=str, help='head bounding boxes', default='data/demo/person1.txt')
-parser.add_argument('--vis_mode', type=str, help='heatmap or arrow', default='heatmap')
-parser.add_argument('--out_threshold', type=int, help='out-of-frame target dicision threshold', default=100)
-args = parser.parse_args()
+class Inference:
+    def __init__(self, model_weights, out_threshold = 200):
+        self.input_resolution = 224
+        self.output_resolution = 64
+        self.image_transform = transforms.Compose(
+            [
+                transforms.Resize((self.input_resolution, self.input_resolution)),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            ]
+        )
+        self.model = ModelSpatial()
+        model_dict = self.model.state_dict()
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        pretrained_dict = torch.load(model_weights, map_location=self.device)
+        pretrained_dict = pretrained_dict['model']
+        model_dict.update(pretrained_dict)
+        self.model.load_state_dict(model_dict)
+        if self.device == 'cuda':
+            self.model.cuda()
+        self.model.train(False)
+
+        self.out_threshold = out_threshold
 
 
-def _get_transform():
-    transform_list = []
-    transform_list.append(transforms.Resize((input_resolution, input_resolution)))
-    transform_list.append(transforms.ToTensor())
-    transform_list.append(transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]))
-    return transforms.Compose(transform_list)
+    def run(self,frame_raw, head_box):
 
 
-def run():
-    column_names = ['frame', 'left', 'top', 'right', 'bottom']
-    df = pd.read_csv(args.head, names=column_names, index_col=0)
-    df['left'] -= (df['right']-df['left'])*0.1
-    df['right'] += (df['right']-df['left'])*0.1
-    df['top'] -= (df['bottom']-df['top'])*0.1
-    df['bottom'] += (df['bottom']-df['top'])*0.1
+        with torch.no_grad():
 
-    # set up data transformation
-    test_transforms = _get_transform()
-
-    model = ModelSpatial()
-    model_dict = model.state_dict()
-    pretrained_dict = torch.load(args.model_weights)
-    pretrained_dict = pretrained_dict['model']
-    model_dict.update(pretrained_dict)
-    model.load_state_dict(model_dict)
-
-    model.cuda()
-    model.train(False)
-
-    with torch.no_grad():
-        for i in df.index:
-            frame_raw = Image.open(os.path.join(args.image_dir, i))
             frame_raw = frame_raw.convert('RGB')
             width, height = frame_raw.size
-
-            head_box = [df.loc[i,'left'], df.loc[i,'top'], df.loc[i,'right'], df.loc[i,'bottom']]
-
             head = frame_raw.crop((head_box)) # head crop
 
-            head = test_transforms(head) # transform inputs
-            frame = test_transforms(frame_raw)
+            head = self.image_transform(head) # transform inputs
+            frame = self.image_transform(frame_raw)
             head_channel = imutils.get_head_box_channel(head_box[0], head_box[1], head_box[2], head_box[3], width, height,
-                                                        resolution=input_resolution).unsqueeze(0)
-
-            head = head.unsqueeze(0).cuda()
-            frame = frame.unsqueeze(0).cuda()
-            head_channel = head_channel.unsqueeze(0).cuda()
+                                                        resolution=self.input_resolution).unsqueeze(0)
+            if self.device == 'cuda':
+                head = head.unsqueeze(0).cuda()
+                frame = frame.unsqueeze(0).cuda()
+                head_channel = head_channel.unsqueeze(0).cuda()
+            else:
+                head = head.unsqueeze(0)
+                frame = frame.unsqueeze(0)
+                head_channel = head_channel.unsqueeze(0)
 
             # forward pass
-            raw_hm, _, inout = model(frame, head_channel, head)
+            raw_hm, _, inout = self.model(frame, head_channel, head)
 
             # heatmap modulation
             raw_hm = raw_hm.cpu().detach().numpy() * 255
@@ -82,34 +71,54 @@ def run():
             inout = inout.cpu().detach().numpy()
             inout = 1 / (1 + np.exp(-inout))
             inout = (1 - inout) * 255
-            norm_map = imresize(raw_hm, (height, width)) - inout
 
-            # vis
-            plt.close()
-            fig = plt.figure()
-            fig.canvas.manager.window.move(0,0)
-            plt.axis('off')
-            plt.imshow(frame_raw)
+            # # MyVis
+            head_box = list(map(int, head_box))
+            frame_raw = np.array(frame_raw)
+            cv2.rectangle(frame_raw, (head_box[0], head_box[1]), (head_box[2], head_box[3]), (255, 0, 0), 2)
+            if inout <  self.out_threshold:  # in-frame gaze
+                pred_x, pred_y = evaluation.argmax_pts(raw_hm)
+                norm_p = [pred_x / self.output_resolution, pred_y / self.output_resolution]
 
-            ax = plt.gca()
-            rect = patches.Rectangle((head_box[0], head_box[1]), head_box[2]-head_box[0], head_box[3]-head_box[1], linewidth=2, edgecolor=(0,1,0), facecolor='none')
-            ax.add_patch(rect)
 
-            if args.vis_mode == 'arrow':
-                if inout < args.out_threshold: # in-frame gaze
-                    pred_x, pred_y = evaluation.argmax_pts(raw_hm)
-                    norm_p = [pred_x/output_resolution, pred_y/output_resolution]
-                    circ = patches.Circle((norm_p[0]*width, norm_p[1]*height), height/50.0, facecolor=(0,1,0), edgecolor='none')
-                    ax.add_patch(circ)
-                    plt.plot((norm_p[0]*width,(head_box[0]+head_box[2])/2), (norm_p[1]*height,(head_box[1]+head_box[3])/2), '-', color=(0,1,0,1))
-            else:
-                plt.imshow(norm_map, cmap = 'jet', alpha=0.2, vmin=0, vmax=255)
-
-            plt.show(block=False)
-            plt.pause(0.2)
-
-        print('DONE!')
-
+                starting_point = ((head_box[0] + head_box[2]) // 2, (head_box[1] + head_box[3]) // 2)
+                ending_point = (int(norm_p[0] * width), int(norm_p[1] * height))
+                return starting_point, ending_point
+            return None, None
 
 if __name__ == "__main__":
-    run()
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('--model_weights', type=str, help='model weights', default='trained_model/epoch_18_weights.pt')
+    # parser.add_argument('--image_dir', type=str, help='images', default='data/gaze_follow_test/frames')
+    # parser.add_argument('--head', type=str, help='head bounding boxes', default='data/gaze_follow_test/test.txt')
+    # args = parser.parse_args()
+    MODEL_WEIGHTS = 'trained_model/epoch_18_weights.pt'
+    IMAGE_DIR = 'data/gaze_follow_test/frames'
+    HEAD = 'data/gaze_follow_test/test.txt'
+
+    obj = Inference(MODEL_WEIGHTS)
+
+    column_names = ['frame', 'left', 'top', 'right', 'bottom']
+    df = pd.read_csv(HEAD, names=column_names, index_col=0)
+    df['left'] -= (df['right'] - df['left']) * 0.1
+    df['right'] += (df['right'] - df['left']) * 0.1
+    df['top'] -= (df['bottom'] - df['top']) * 0.1
+    df['bottom'] += (df['bottom'] - df['top']) * 0.1
+    for i in df.index:
+        frame_raw = Image.open(os.path.join(IMAGE_DIR, i))
+        head_box = [df.loc[i, 'left'], df.loc[i, 'top'], df.loc[i, 'right'], df.loc[i, 'bottom']]
+        starting_point, ending_point = obj.run(frame_raw, head_box)
+        img_cp = np.array(frame_raw)
+        if starting_point != None and ending_point != None:
+
+            cv2.line(img_cp, starting_point, ending_point, (255, 0, 0), 5)
+            cv2.circle(img_cp,ending_point, 15,
+                       (255, 0, 0), -1)
+        screen = cv2.cvtColor(img_cp, cv2.COLOR_RGB2BGR)
+        cv2.imshow('Frame', screen)
+
+        # Press Q on keyboard to  exit
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
+    print('DONE!')
+
