30de5d7726a94f3b7586ad26a23107efc6560502
talha zubair <94974993+maliktalha1996@users.noreply.github.com>
2023-01-17 19:37:26 +0500

    Adding changes with multiple heads

--- demo.py
+++ demo.py
@@ -13,7 +13,7 @@ from cv2 import resize as imresize
 from model import ModelSpatial
 from utils import imutils, evaluation
 
-import cv2
+import cv2, random
 
 class Inference:
     def __init__(self, model_weights, out_threshold = 200):
@@ -40,7 +40,7 @@ class Inference:
         self.out_threshold = out_threshold
 
 
-    def run(self,frame_raw, head_box):
+    def runOverSingle(self,frame_raw, head_box):
 
 
         with torch.no_grad():
@@ -85,6 +85,75 @@ class Inference:
                 ending_point = (int(norm_p[0] * width), int(norm_p[1] * height))
                 return starting_point, ending_point
             return None, None
+    def makeBatch(self, frame_raw, cmbHeadBox, width, height):
+        head_list = []
+        head_channel_list = []
+        frame_list = []
+        for iter, head_box in enumerate(cmbHeadBox):
+            head = frame_raw.crop((head_box))  # head crop
+
+            head = self.image_transform(head)  # transform inputs
+
+            frame = self.image_transform(frame_raw)
+
+            head_channel = imutils.get_head_box_channel(head_box[0], head_box[1], head_box[2], head_box[3], width, height,
+                                                        resolution=self.input_resolution).unsqueeze(0)
+            head_list.append(head)
+            head_channel_list.append(head_channel)
+            frame_list.append(frame)
+        return head_list, frame_list, head_channel_list
+    def runOverBatch(self,frame_raw, cmbHeadBox):
+
+
+        with torch.no_grad():
+
+            frame_raw = frame_raw.convert('RGB')
+            width, height = frame_raw.size
+
+            head, frame,head_channel = self.makeBatch(frame_raw, cmbHeadBox, width, height)
+            head = torch.stack(head)
+            frame = torch.stack(frame)
+            head_channel = torch.stack(head_channel)
+
+            # head = frame_raw.crop((head_box)) # head crop
+
+            # head = self.image_transform(head) # transform inputs
+
+            # head_channel = imutils.get_head_box_channel(head_box[0], head_box[1], head_box[2], head_box[3], width, height,
+            #                                             resolution=self.input_resolution).unsqueeze(0)
+            if self.device == 'cuda':
+                head = head.cuda()
+                frame = frame.cuda()
+                head_channel = head_channel.cuda()
+            else:
+                head = head
+                frame = frame
+                head_channel = head_channel
+
+            # forward pass
+            raw_hms, _, inouts = self.model(frame, head_channel, head)
+
+            return_pnts = []
+            for raw_hm, inout, head_box in zip(raw_hms, inouts, cmbHeadBox):
+                # heatmap modulation
+                raw_hm = raw_hm.cpu().detach().numpy() * 255
+                raw_hm = raw_hm.squeeze()
+                inout = inout.cpu().detach().numpy()
+                inout = 1 / (1 + np.exp(-inout))
+                inout = (1 - inout) * 255
+
+                # # MyVis
+                # head_box = list(map(int, head_box))
+                # frame_raw = np.array(frame_raw)
+                # cv2.rectangle(frame_raw, (head_box[0], head_box[1]), (head_box[2], head_box[3]), (255, 0, 0), 2)
+                if inout <  self.out_threshold:  # in-frame gaze
+                    pred_x, pred_y = evaluation.argmax_pts(raw_hm)
+                    norm_p = [pred_x / self.output_resolution, pred_y / self.output_resolution]
+                    starting_point = ((head_box[0] + head_box[2]) // 2, (head_box[1] + head_box[3]) // 2)
+                    ending_point = (int(norm_p[0] * width), int(norm_p[1] * height))
+                    return_pnts.append([starting_point, ending_point])
+                return_pnts.append([None, None])
+            return return_pnts
 
 if __name__ == "__main__":
     # parser = argparse.ArgumentParser()
@@ -93,8 +162,8 @@ if __name__ == "__main__":
     # parser.add_argument('--head', type=str, help='head bounding boxes', default='data/gaze_follow_test/test.txt')
     # args = parser.parse_args()
     MODEL_WEIGHTS = 'trained_model/epoch_18_weights.pt'
-    IMAGE_DIR = 'data/gaze_follow_test/frames'
-    HEAD = 'data/gaze_follow_test/test.txt'
+    IMAGE_DIR = 'data/elm_gaze/frames'
+    HEAD = 'data/elm_gaze/test.txt'
 
     obj = Inference(MODEL_WEIGHTS)
 
@@ -104,21 +173,52 @@ if __name__ == "__main__":
     df['right'] += (df['right'] - df['left']) * 0.1
     df['top'] -= (df['bottom'] - df['top']) * 0.1
     df['bottom'] += (df['bottom'] - df['top']) * 0.1
-    for i in df.index:
-        frame_raw = Image.open(os.path.join(IMAGE_DIR, i))
-        head_box = [df.loc[i, 'left'], df.loc[i, 'top'], df.loc[i, 'right'], df.loc[i, 'bottom']]
-        starting_point, ending_point = obj.run(frame_raw, head_box)
-        img_cp = np.array(frame_raw)
-        if starting_point != None and ending_point != None:
 
-            cv2.line(img_cp, starting_point, ending_point, (255, 0, 0), 5)
-            cv2.circle(img_cp,ending_point, 15,
-                       (255, 0, 0), -1)
+    df_grpd = df.reset_index().groupby(['frame'])
+    for ind, row in df_grpd:
+        cmb_head_bbox = []
+        for d in row.index.tolist():
+            cmb_head_bbox.append([row.loc[d, 'left'],
+                  row.loc[d, 'top'],
+                  row.loc[d, 'right'],
+                  row.loc[d, 'bottom']])
+
+
+        frame_raw = Image.open(os.path.join(IMAGE_DIR, ind))
+        return_pnts = obj.runOverBatch(frame_raw, cmb_head_bbox)
+        img_cp = np.array(frame_raw)
+
+        for starting_point, ending_point in return_pnts:
+            print(starting_point, ending_point)
+            if starting_point != None and ending_point != None:
+                # r = random.randint(0, 255)
+                # g = random.randint(0, 255)
+                # b = random.randint(0, 255)
+                b= 255; g = 0; r = 0
+                cv2.line(img_cp, tuple(map(int,starting_point)), tuple(map(int,ending_point)), (b, g, r), 5)
+                cv2.circle(img_cp, tuple(map(int,ending_point)), 15,
+                           (b, g, r), -1)
         screen = cv2.cvtColor(img_cp, cv2.COLOR_RGB2BGR)
         cv2.imshow('Frame', screen)
 
         # Press Q on keyboard to  exit
         if cv2.waitKey(25) & 0xFF == ord('q'):
             break
+
+    # for i in df.index:
+    #     frame_raw = Image.open(os.path.join(IMAGE_DIR, i))
+    #     head_box = [df.loc[i, 'left'], df.loc[i, 'top'], df.loc[i, 'right'], df.loc[i, 'bottom']]
+    #     starting_point, ending_point = obj.run(frame_raw, head_box)
+    #     img_cp = np.array(frame_raw)
+    #     if starting_point != None and ending_point != None:
+    #         cv2.line(img_cp, starting_point, ending_point, (255, 0, 0), 5)
+    #         cv2.circle(img_cp,ending_point, 15,
+    #                    (255, 0, 0), -1)
+    #     screen = cv2.cvtColor(img_cp, cv2.COLOR_RGB2BGR)
+    #     cv2.imshow('Frame', screen)
+    #
+    #     # Press Q on keyboard to  exit
+    #     if cv2.waitKey(25) & 0xFF == ord('q'):
+    #         break
     print('DONE!')
 
